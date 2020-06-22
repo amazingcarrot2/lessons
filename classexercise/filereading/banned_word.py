ff72c6f2a972fe87230a39f9b11430ebbbd2e7d1
Suki <suki.hu@mrs.ai>
2020-06-23 00:38:06 +0800

    格式的整理

--- classexercise/filereading/banned_word.py
+++ classexercise/filereading/banned_word.py
@@ -1,22 +1,24 @@
 # encoding=utf-8
 import jieba
 
-#读取屏蔽词文件
+
+# 读取屏蔽词文件
 def get_banned(bannedfile):
     bannedlist = []
-    #可以选取指定文件作为屏蔽词
+    # 可以选取指定文件作为屏蔽词
     with open('%s.txt' % bannedfile, 'r') as word:
         banned_word = word.readlines()
-        #print(banned_word)
-    #将屏蔽词加入到新的list中输出，并加入jieba分词词库
+        # print(banned_word)
+    # 将屏蔽词加入到新的list中输出，并加入jieba分词词库
     for word in banned_word:
         word = word.strip()
         bannedlist.append(word)
         jieba.add_word(word)
-    #print(bannedlist)
+    # print(bannedlist)
     return bannedlist
 
-#将输入的句子用jieba进行分词
+
+# 将输入的句子用jieba进行分词
 def splitword(rawdata, bannedfile):
     bannedlist = get_banned(bannedfile)
     raw_list = jieba.lcut(rawdata)
@@ -32,4 +34,3 @@ def splitword(rawdata, bannedfile):
 
 rawdata = input('input your sentence:')
 splitword(rawdata, 'banned_word')
-
